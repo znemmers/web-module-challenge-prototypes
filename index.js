511c7bf651caa535701f7627f967291e43ed28b0
Zach Nemmers <Zach@Zachs-MacBook-Pro-3.local>
2022-12-06 19:17:13 -0600

    completed MVP

--- index.js
+++ index.js
@@ -15,10 +15,29 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `this ${this.name}, ${this.age}`;
 }
 
+const mary = new Person('Mary', 50);
+console.log(mary.toString())
+mary.eat('pizza');
+console.log(mary.stomach);
+mary.poop();
+console.log(mary.stomach)
 
 /*
   TASK 2
@@ -36,8 +55,25 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, mpg) {
+  this.model = model;
+  this.milesPerGallon = mpg;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function(dist){
+  const driveableMiles = this.tank * this.milesPerGallon;
+  if(dist <= driveableMiles){
+    this.odometer = this.odometer + dist;
+    this.tank = this.tank - (dist / this.milesPerGallon);
+  }else{
+    this.odometer = this.odometer + driveableMiles;
+    this.take = 0;
+    return `I ran out of fuel at ${this.odometer} miles!`; 
+  }
 }
 
 
@@ -49,18 +85,23 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}.`
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. when the scope is global, 'this' = the window/console Object
+  2. when a preceding dot calls a function, the object to the left of dot is 'this'
+  3. when a constructor function is used, 'this' refers to the new Object created by the constructor function
+  4. when JS call and/or apply methods are used, 'this' is explicitly defined
 */
 
 ///////// END OF CHALLENGE /////////
